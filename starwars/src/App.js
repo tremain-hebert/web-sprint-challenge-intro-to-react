dedcc6ea6e115465265888e489e11eddc4c8b5d2
Truman <truman@Trumans-iMac.local>
2020-06-28 20:38:38 -0500

    Created Character Card

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
+  const [characterList, setCharacterList] = useState([]);
+  useEffect(() => {
+    axios
+      .get('https://rickandmortyapi.com/api/character/')
+      .then(res => {
+        console.log('This is the res from useEffect of CharacterList: ', res)
+        setCharacterList(res.data.results);
+      })
+      .catch(err => {
+        console.log('There was an error in useEffect of CharacterList: ', err)
+      })
+  }, []);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +25,17 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {characterList.map(character => {
+        return
+          <Character
+            key={character.id}
+            name={character.name}
+            status={character.status}
+            species={character.species}
+            gender={character.gender}
+            image={character.image}
+            />
+      })}
     </div>
   );
 }
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,14 @@
-// Write your Character component here
+import React from 'react';
+
+const Character = ({ name, status, species, gender, image }) => {
+    return (
+        <div className='charater-list'>
+            <h2>{name}</h2>
+            <img src={image} alt={name}/>
+            <p>{status}</p>
+            <p>{species}</p>
+            <p>{gender}</p>
+        </div>
+    );
+};
+export default Character;
